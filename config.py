e96a2ae29cdbb2552776ade03251aa687d3dc20d
Andrei Arkhipov <it.arkhipov@gmail.com>
2023-12-21 22:24:25 +0300

    refactor

--- config.py
+++ config.py
@@ -4,7 +4,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-context = os.getenv('context', 'bstack')
 user_name = os.getenv('user_name')
 access_key = os.getenv('access_key')
-remote_browser_url = os.getenv('remote_browser_url')
--- conftest.py
+++ conftest.py
@@ -26,26 +26,19 @@ def android_mobile():
         }
     })
 
-    with allure.step('init app session'):
+    with allure.step('Initialization'):
         browser.config.driver = webdriver.Remote(
             "http://hub.browserstack.com/wd/hub",
             options=options
         )
-
     browser.config.timeout = float(os.getenv('timeout', '10.0'))
-
-    browser.config._wait_decorator = support._logging.wait_with(
-        context=allure_commons._allure.StepContext
-    )
+    browser.config._wait_decorator = support._logging.wait_with(context=allure_commons._allure.StepContext)
 
     yield
 
     attach.screenshot()
     attach.page_source()
-
     session_id = browser.driver.session_id
-
-    with allure.step('tear down app session'):
+    with allure.step('Close application'):
         browser.quit()
-
     attach.video(session_id)
